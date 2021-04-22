497e41b1024e96f31902055929d41c82fab50678
Andre Macola <andremacola@gmail.com>
2021-04-22 13:35:17 -0300

    cache-control

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -5,6 +5,8 @@ async function Tempo(request, response) {
 	const wpPosts = await fetch(process.env.POSTS_API_URL)
 	const posts = await wpPosts.json()
 
+	response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
 	response.json({
 		date: dynamicDate.toGMTString(),
 		posts
